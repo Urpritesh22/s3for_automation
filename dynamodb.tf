d1b1801c65fe75003bbf75453070e4be08cb3c34
Pritesh Chopade <121218533+Urpritesh22@users.noreply.github.com>
2024-06-08 14:56:48 +0530

    Update dynamodb.tf

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,3 +1,13 @@
+variable "dynamodb_name" {
+    default = "my-dynamodb-table1"
+  
+}
+variable "hash_key" {
+    default = "LockID"
+    type = string
+  
+}
+
 resource "aws_dynamodb_table" "our-table" {
     name = var.my-dynamodb-table1
     read_capacity = 5
